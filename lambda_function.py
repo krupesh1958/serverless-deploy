886bb63f2fd24c0d71ede61429b171d3776cb703
krupesh patel <krupesh.patel@yudiz.com>
2024-02-13 15:13:57 +0530

    fix: created lambda function url and update it into the project url.

--- lambda_function.py
+++ lambda_function.py
@@ -16,43 +16,24 @@ def lambda_handler(event, context):
     if auth == "allow":
         effect = "Allow"
 
-    auth_resp = generate_policy(
-        principal_id="user",
-        effect=effect,
-        resource=event["methodArn"]
-    )
+    auth_resp = {
+        "principalId": "user",
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "execute-api.invoke",
+                    "Effect": effect,
+                    "Resource": "arn:aws:execute-api:us-east-1:434634485527:ern6h3gdac/*/*/*"
+                }
+            ]
+        },
+        "context": {
+            "stringKey": "value",
+            "numberKey": "1",
+            "booleanKey": "true"
+        },
+        "usageIdentifierKey": "{api-key}"
+    }
     return auth_resp
 
-
-def generate_policy(
-    principal_id: str,
-    effect: str,
-    resource: str
-) -> json:
-    """
-    `Is authentication token is valid then how to pass message to api-gateway to
-    understand that hey, this token is valid please go future process.`
-    This function has solved this questions.
-    """
-    auth_resp = {
-        "principalId": principal_id
-    }
-    if effect and resource:
-        policy = {
-            "Version": "2012-10-17",
-            "Statement": []
-        }
-        statement = {
-            "Action": "execute-api:Invoke",
-            "Effect": effect,
-            "Resource": resource
-        }
-        policy["Statement"] = [statement]
-        auth_resp["policyDocument"] = policy
-    
-    auth_resp["context"] = {
-        "stringKey": "stringval",
-        "numberKey": 123,
-        "booleanKey": True
-    }
-    return json.dumps(auth_resp)
